ca6d16b50d4ed401050e0e1d8dfe84059d378b52
Matt Persell-Thompson <mattypt@hotmail.co.uk>
2022-11-28 21:05:14 +0000

    feat: Added dropdown

--- build/js/script.js
+++ build/js/script.js
@@ -1,5 +1,16 @@
 const dealLink = Array.from(document.querySelectorAll(".deal__link__container"));
 const dealBanner = Array.from(document.querySelectorAll(".banner__container"));
+const openDropdown = document.querySelector(".dropdown--open");
+const dropdownContainer = document.querySelector(".dropdown__container");
+const closeDropdown = document.querySelector(".dropdown--close");
+
+openDropdown.addEventListener("click", () => {
+    dropdownContainer.classList.add("dropdown--activated");
+});
+
+closeDropdown.addEventListener("click", () => {
+    dropdownContainer.classList.remove("dropdown--activated");
+})
 
 
 for(let i = 0; i <= dealLink.length; i++) {
--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,15 @@
 
 var dealLink = Array.from(document.querySelectorAll(".deal__link__container"));
 var dealBanner = Array.from(document.querySelectorAll(".banner__container"));
+var openDropdown = document.querySelector(".dropdown--open");
+var dropdownContainer = document.querySelector(".dropdown__container");
+var closeDropdown = document.querySelector(".dropdown--close");
+openDropdown.addEventListener("click", function () {
+  dropdownContainer.classList.add("dropdown--activated");
+});
+closeDropdown.addEventListener("click", function () {
+  dropdownContainer.classList.remove("dropdown--activated");
+});
 var _loop = function _loop(i) {
   dealLink[i].addEventListener("mouseover", function () {
     dealBanner[i].classList.add("active--banner");
